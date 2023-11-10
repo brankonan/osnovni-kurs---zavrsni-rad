35f75461fcb2602ebfa67157cd55295319bde91e
brankonan <branko.kovacevic09@gmail.com>
2023-11-10 10:43:27 +0100

    zadatak5

--- comments.php
+++ comments.php
@@ -1,4 +1,23 @@
 <?php
 include 'header.php';
-include 'footer.php';
+?>
+<div class="comments">
+    <hr>
+    <h3>Comments</h3>
+    <ul>
+        <?php foreach ($comments as $comment) { ?>
+            <li>
+                <h4>
+                    <?= $comment['author'] ?>
+                </h4>
+                <?= $comment['text'] ?>
+            </li>
+        <?php } ?>
+    </ul>
+</div>
+<?php
 include 'sidebar.php';
+?>
+<?php
+include 'footer.php';
+?>
