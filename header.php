a26cbe9042fd4045f3b9f638a7ce5b4d28c35323
brankonan <branko.kovacevic09@gmail.com>
2023-11-10 10:03:07 -0800

    header ispravka

--- header.php
+++ header.php
@@ -1,8 +1,14 @@
-<div class="blog-masthead">
-            <div class="container">
-                <nav class="nav">
-                    <a class="nav-link active" href="#">Home</a>
-                    <a class="nav-link active" href="index.php">Home</a>
-                    <a class="nav-link" href="#">New features</a>
-                    <a class="nav-link" href="#">Press</a>
-                    <a class="nav-link" href="#">New hires</a>
+
+<body>
+  <header>
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="nav">
+          <a class="nav-link active" href="#">Home</a>
+          <a class="nav-link" href="#">New features</a>
+          <a class="nav-link" href="#">Press</a>
+          <a class="nav-link" href="#">New hires</a>
+          <a class="nav-link" href="#">About</a>
+        </nav>
+      </div>
+    </div>
