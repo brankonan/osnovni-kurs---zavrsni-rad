9cb4cef57f70779badfc6fc7af4bc2b684a54a5c
brankonan <branko.kovacevic09@gmail.com>
2023-11-10 10:41:26 +0100

    zadatak5

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+//zadatak 4
 mysql -u root
 
 show databases;
@@ -21,4 +22,26 @@ INSERT INTO posts (title, body, author) VALUES ('Naslov 2', 'Tekst drugog posta.
 
 INSERT INTO posts (title, body, author) VALUES ('Naslov 3', 'Tekst trećeg posta.', 'Autor 1');
 
-desc posts;
+select * from posts;
+
+//zadatak 5
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    author VARCHAR(255) NOT NULL,
+    text TEXT NOT NULL,
+    post_id INT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts(id));
+
+    desc comments;
+
+    INSERT INTO comments (author, text, post_id)
+VALUES ('Korisnik 1', 'Ovo je prvi komentar na post', 1);
+
+
+INSERT INTO comments (author, text, post_id)
+VALUES ('Korisnik 2', 'Ovo je drugi komentar na post', 1);
+
+INSERT INTO comments (author, text, post_id)
+VALUES ('Korisnik 3', 'Ovo je treći komentar na post', 2);
+
+select * from comments;
