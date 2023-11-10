2e26a3665d3c98c5bfc65363067bab16129fd95f
brankonan <branko.kovacevic09@gmail.com>
2023-11-10 10:49:40 +0100

    zadatak6

--- comments.php
+++ comments.php
@@ -2,7 +2,20 @@
 include 'header.php';
 ?>
 <div class="comments">
+<div class="comments">
     <hr>
+    <button class="btn btn-default" id="comments-btn">Hide comments</button>
+    <h3>Comments</h3>
+    <ul>
+    <ul id="comments-list">
+        <?php foreach ($comments as $comment) { ?>
+            <li>
+            <li class="">
+                <h4>
+                    <?= $comment['author'] ?>
+                </h4>   
+                <?php } ?> 
+<hr>
     <h3>Comments</h3>
     <ul>
         <?php foreach ($comments as $comment) { ?>
