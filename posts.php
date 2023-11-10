00c9859c364302c37944b77b020db398999d5a13
brankonan <branko.kovacevic09@gmail.com>
2023-11-10 14:59:17 +0100

    ispravka posts

--- posts.php
+++ posts.php
@@ -1,25 +1,25 @@
 <?php
-include 'header.php';
-
-include 'sidebar.php';
-
-
-
-
 include('db_connection.php'); 
+include 'header.php';
 
 
 $query = "SELECT * FROM posts ORDER BY Created_at DESC"; 
 $statement = $connection->query($query);
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+<?php
+foreach ($posts as $post):?>
 
-foreach ($posts as $post){
- echo "<div class='post'>";
- echo "<h2>" . $post['title']. "</h2>";
- echo "<p>" . $post['body']. "</p>";
- echo "<p>Author: " . $post['author'] . '</p>';
- echo "</div>";
-}
+<div class='post'>
+ <a href=/osnovni-kurs---zavrsni-rad/single-post.php?post_id=" <?= $post['id'] ?> ">
+    <h2> <?= $post['title'] ?> </h2>
+</a> 
+ <p> <?= $post['body']?> </p>
+ <p> <?= "Author: " . $post['author'] ?> </p>
+</div>
+<?php endforeach; ?>
 
+<?php
+include 'sidebar.php';
 include 'footer.php';
 ?>
